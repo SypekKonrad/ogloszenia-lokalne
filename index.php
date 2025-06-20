88fbfc5a3ebaeef64d0058a53dc030ac746596d8
Konrad Sypek <konrad.sypek@o2.pl>
2025-06-20 15:59:18 +0200

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>landing page</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+        <!-- navbar -->
+    <nav>
+        <a href="register.php" >Sign Up</a>
+        <a href="login.php">Sign In</a>
+    </nav>
+
+    <main>
+
+        <!-- jakies tresci -->
+        <div>
+            <p>jakies tresci landing page</p>
+        </div>
+
+    </main>
+</body>
+</html>
+
+<!-- class="wybrany" -->
